f71c652ba1cae15b5f01dbc134a70252360e8d9f
Steren <steren.giannini@gmail.com>
2014-03-05 16:12:17 -0500

    drag and drop support, new style, filename stripping, local crypto library

--- prioprepare.js
+++ prioprepare.js
@@ -1,9 +1,9 @@
-
+/*global CryptoJS */
 
 if (window.File && window.FileReader) {
   // Great success! All the File APIs are supported.
 } else {
-  alert('Sorry, your brower does not support.');
+  alert('Sorry, your brower does not support features needed for this service.');
 }
 
 
@@ -14,9 +14,7 @@ var hashFile = function(fileAsText) {
 };
 
 
-function handleFileSelect(evt) {
-  var files = evt.target.files; // FileList object
-  
+function handleFiles(/* FileList */ files) {
   if(files.length === 0) {
     // no file selected
     return;
@@ -30,19 +28,87 @@ function handleFileSelect(evt) {
     
     var hash = hashFile(e.target.result);
     
-    console.log(hash.length);
-    console.log(hash);
+    var cleanedName = f.name;
+    cleanedName.replace(/[^A-Z0-9.\-]/gi, '');
     
-    document.getElementById('output').innerHTML = hash;
+    var finalText = 'SHA256 hash value ' + hash + ' ' + cleanedName;
     
-    document.getElementById('sliced-output-0').value = hash.substr(0*16, 16);
-    document.getElementById('sliced-output-1').value = hash.substr(1*16, 16);
-    document.getElementById('sliced-output-2').value = hash.substr(2*16, 16);
-    document.getElementById('sliced-output-3').value = hash.substr(3*16, 16);
+    console.log(finalText);
+    
+    document.getElementById('output').value = finalText;
+    document.getElementById('output').classList.add('non-disabled')
+    
+    document.getElementById('todo-next').innerText = 'Copy this entire text in a bank transfert statement.'
+    
+    // document.getElementById('sliced-output-0').value = hash.substr(0*16, 16);
+    // document.getElementById('sliced-output-1').value = hash.substr(1*16, 16);
+    // document.getElementById('sliced-output-2').value = hash.substr(2*16, 16);
+    // document.getElementById('sliced-output-3').value = hash.substr(3*16, 16);
     
   };
   
   reader.readAsText(f);
 }
 
-document.getElementById('file-input').addEventListener('change', handleFileSelect, false);
+var fileInput = document.getElementById('file-input');
+
+var onFileInputChange = function(e) {
+  handleFiles(e.target.files);
+};
+fileInput.addEventListener('change', onFileInputChange, false);
+
+var drophere = document.getElementById('js-drophere');
+var dropbox = document.getElementById("js-dropbox")
+var dropmask = document.getElementById("drop-mask");
+
+var addDropInfo = function() {
+  dropmask.style.display = 'block';
+  
+  // display a "drop here" message
+  drophere.style.display = 'block';
+  drophere.innerHTML = '<strong>Drop</strong> this file anywhere on this window to compute its hash value.';
+
+  // change the dropbox color
+  dropbox.classList.add("dropme");
+};
+
+var removeDropInfo = function() {
+  dropmask.style.display = 'none';
+  drophere.style.display = 'none';
+  drophere.innerHTML = '';
+  dropbox.classList.remove("dropme");
+};
+
+var drop = function (e) {
+  e.stopPropagation();
+  e.preventDefault();
+  removeDropInfo();
+
+  var dt = e.dataTransfer;
+  var files = dt.files;
+
+  // this will trigger a "change" event for the inpur element
+  fileInput.files = files;
+};
+
+var dragover = function(e) {
+  console.log("dragover");
+  
+  e.preventDefault();
+  addDropInfo();
+};
+
+var dragleave = function(e) {
+  console.log("dragleave");
+
+  e.preventDefault();
+  removeDropInfo();
+};
+
+dropbox.addEventListener("dragover", dragover, false);
+dropmask.addEventListener("dragleave", dragleave, false);
+dropmask.addEventListener("drop", drop, false);
+
+
+
+
